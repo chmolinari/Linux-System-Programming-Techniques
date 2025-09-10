5df68f8745a6581b3353fb464b00a8f678fe16a7
Christian Molinari <chmolinari@christianmolinari.com>
2025-09-10 23:29:12 +0200

    Added errors handling

--- main.cpp
+++ main.cpp
@@ -15,7 +15,15 @@ int main(int argc, char **argv) {
     int input_numbers = argc - 1;
     inputs.resize(input_numbers);
     for (int i = 0; i < input_numbers; i++) {
-        inputs[i] = std::stoi(argv[i + 1]);
+        try {
+            inputs[i] = std::stoi(argv[i + 1]);
+        } catch (std::invalid_argument &e) {
+            std::cout << "Failed parsing argument " << argv[i + 1] << ". All arguments must be integer numbers!\n";
+            return EXIT_FAILURE;
+        } catch (std::out_of_range &e) {
+            std::cout << "Argument " << argv[i + 1] << " is too big to be processed!\n";
+            return EXIT_FAILURE;
+        }
     }
 
     int result = 0;
